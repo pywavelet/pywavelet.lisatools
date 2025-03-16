9b1185d0e1ebd1581f7976a1ba9a1428286ec5c5
avi <avi.vajpeyi@gmail.com>
2025-03-17 12:15:28 +1300

    add testing tools

--- src/lisatools_wdm/mcmc/setup.py
+++ src/lisatools_wdm/mcmc/setup.py
@@ -1,20 +1,16 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
-
+import dataclasses
 from fastlisaresponse import ResponseWrapper
 from lisatools.sensitivity import A1TDISens, E1TDISens
 from lisatools.detector import EqualArmlengthOrbits
-
 from lisatools_wdm.datacontainer import DataResidualArray
 from lisatools_wdm.sensitivity import SensitivityMatrix
 from lisatools_wdm.analysiscontainer import AnalysisContainer
-from tqdm.auto import tqdm, trange
-
 from lisatools_wdm.waveform import GBWave
-from lisatools_wdm.plotting import plot_ae_time_domain, plot_ae_freq_domain
-
-import dataclasses
-
+from lisatools_wdm.plotting import plot_ae_time_domain, plot_ae_freq_domain, plot_signal_on_characteristic_strain
+from tqdm.auto import tqdm, trange
 
 @dataclasses.dataclass
 class MCMCData:
@@ -43,30 +39,36 @@ class MCMCData:
     sens_mat: SensitivityMatrix
     analysis: AnalysisContainer
 
-
-
-def setup(use_gpu = False):
-    gb = GBWave(use_gpu)
-
-    T = 2.0  # years
-    t0 = 100000.0  # time at which signal starts (chops off data at start of waveform where information is not correct)
-
-    sampling_frequency = 0.01
+def setup(
+    use_gpu=False,
+    T=2.0,
+    t0=100000.0,
+    sampling_frequency=0.01,
+    order=25,
+    tdi_gen="2nd generation",
+    index_lambda=6,
+    index_beta=7,
+    Nf=64,
+    A=1.084702251e-22,
+    f=2.35962078e-3,
+    fdot=1.47197271e-17,
+    iota=1.11820901,
+    phi0=4.91128699,
+    psi=2.3290324,
+    beta=0.9805742971871619,
+    lam=5.22979888
+):
+    default_args = [A, f, fdot, iota, phi0, psi, lam, beta]
     dt = 1 / sampling_frequency
 
-    # order of the langrangian interpolation
-    order = 25
+    tdi_kwargs_esa = {
+        "order": order,
+        "tdi": tdi_gen,
+        "tdi_chan": "AE",
+    }
 
-    # 1st or 2nd or custom (see docs for custom)
-    tdi_gen = "2nd generation"
-
-    index_lambda = 6
-    index_beta = 7
-
-    tdi_kwargs_esa = dict(
-        order=order, tdi=tdi_gen, tdi_chan="AE",
-    )
-
+    gb = GBWave(use_gpu)
+    char_strain = gb(A, f, fdot, iota, phi0, psi, T=T, dt=dt)
     gb_lisa_esa = ResponseWrapper(
         gb,
         T,
@@ -74,37 +76,15 @@ def setup(use_gpu = False):
         index_lambda,
         index_beta,
         t0=t0,
-        flip_hx=False,  # set to True if waveform is h+ - ihx
+        flip_hx=False,
         use_gpu=use_gpu,
-        remove_sky_coords=True,  # True if the waveform generator does not take sky coordinates
-        is_ecliptic_latitude=True,  # False if using polar angle (theta)
-        remove_garbage=True,  # removes the beginning of the signal that has bad information
+        remove_sky_coords=True,
+        is_ecliptic_latitude=True,
+        remove_garbage=True,
         orbits=EqualArmlengthOrbits(),
         **tdi_kwargs_esa,
     )
 
-    Nf = 64
-
-    # define GB parameters
-    A = 1.084702251e-22
-    f = 2.35962078e-3
-    fdot = 1.47197271e-17
-    iota = 1.11820901
-    phi0 = 4.91128699
-    psi = 2.3290324
-
-    beta = 0.9805742971871619
-    lam = 5.22979888
-    default_args = [
-        A,
-        f,
-        fdot,
-        iota,
-        phi0,
-        psi,
-        lam,
-        beta,
-    ]
 
     ae_data = gb_lisa_esa(*default_args)
     data = DataResidualArray(ae_data, dt=dt, Nf=Nf)
@@ -117,8 +97,7 @@ def setup(use_gpu = False):
         dt=dt
     )
     analysis = AnalysisContainer(data, sens_mat, signal_gen=gb_lisa_esa, Nf=Nf)
-    snr = analysis.snr()
-
+    plot_signal_on_characteristic_strain(char_strain, dt, "char_strain.png")
     plot_ae_time_domain(ae_data, dt, "tdi_time_domain.png")
     plot_ae_freq_domain(analysis, "tdi_freq_domain.png")
     fig, ax = analysis.plot_wdm()
--- src/lisatools_wdm/plotting.py
+++ src/lisatools_wdm/plotting.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from lisatools.sensitivity import get_sensitivity
+from scipy.signal import periodogram
+
 from .waveform import YRSID_SI
 
 def plot_ae_time_domain(ae_data, dt, fname="tdi_timedomain.png"):
@@ -22,3 +25,12 @@ def plot_ae_freq_domain(analysis, fname="tdi_freqdomain.png"):
     plt.savefig(fname)
     plt.close(fig)
 
+def plot_signal_on_characteristic_strain(char_strain, dt, fname="char_strain.png"):
+    fn = np.logspace(-5, -1, 10000)
+    Sn_char_strain = get_sensitivity(fn, sens_fn="LISASens", return_type="char_strain")
+    plt.figure()
+    plt.loglog(fn, Sn_char_strain)
+    f, Pxx = periodogram(char_strain, 1/dt, nfft=2**14)
+    plt.loglog(f, np.sqrt(Pxx))
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Characteristic Strain")
