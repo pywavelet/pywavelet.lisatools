33f0876eb5d71c5d3669552fb38a430bb4ea54d9
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-03-17 17:26:39 +1300

    add lisa tools

--- src/lisatools_wdm/mcmc/emcee_analysis.py
+++ src/lisatools_wdm/mcmc/emcee_analysis.py
@@ -2,6 +2,7 @@ import emcee
 from .setup import setup, wrapper_likelihood, MCMCData
 import numpy as np
 
+from scipy.stats import norm
 
 
 
@@ -9,7 +10,7 @@ import numpy as np
 
 
 
-def run_emcee(mcmc_data:MCMCData, nwalkers=10, nsteps=1000):
+def run_emcee(mcmc_data:MCMCData, nwalkers=10, nsteps=400, wdm=False):
 
     prior_bounds = mcmc_data.prior_bounds
     trues = mcmc_data.true
@@ -19,22 +20,26 @@ def run_emcee(mcmc_data:MCMCData, nwalkers=10, nsteps=1000):
 
 
     def ln_prior(params, fixed_parameters):
+        lnp = 0.0
         for i, (low, high) in enumerate(prior_bounds):
-            if not (low < params[i] < high):
-                return -np.inf
-        return 0.0
+            # if not (low < params[i] < high):
+            #     return -np.inf
+            std = (high - low) / 4
+            mu = (high + low) / 2
+            lnp += norm.logpdf(params[i], mu, std)
+        return lnp
 
-    def ln_prob(params, fixed_parameters, analysis):
+    def ln_prob(params, fixed_parameters, analysis,wdm):
         lp = ln_prior(params, fixed_parameters)
         if not np.isfinite(lp):
             return -np.inf
-        return lp + wrapper_likelihood(params, fixed_parameters, analysis)
+        return lp + wrapper_likelihood(params, fixed_parameters, analysis, wdm)
 
     sampler = emcee.EnsembleSampler(
         nwalkers,
         ndim=ndim,
         log_prob_fn=ln_prob,
-        args=(fixed_parameters, analysis),
+        args=(fixed_parameters, analysis, wdm),
     )
     mcmc_start = trues + 1e-7 * np.random.randn(nwalkers, ndim)
     sampler.run_mcmc(mcmc_start, nsteps, progress=True)
